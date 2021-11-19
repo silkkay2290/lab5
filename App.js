ed2ec3d25d365f36588a2776e488eff07f6fe52c
silkykay <mlt6kps@virginia.edu>
2021-11-19 09:13:57 -0500

    new changes

--- App.js
+++ App.js
@@ -2,14 +2,48 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {joke: "Wait for joke"} //sets state to string-- Wait for joke-- named joke
+  }
+
+  componentDidMount() { //only fired once in the begining 
+    this.getRandomJoke()
+    setInterval(() => {
+        this.getRandomJoke()
+      }, 7000);
+
+  }
+
+
+ async getRandomJoke() {
+    //fetch api returns a promise so wait
+    let response = await fetch("http://api.icndb.com/jokes/random?exclude=[nerdy]&firstName=Prof&lastName=X") //&adds new parameter
+    //.json just returns the json
+    let parsedObject = await response.json()
+    this.setState({
+      joke: parsedObject.value.joke //got from structure of object returned
+    }) //stores new joke
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>
+          {this.state.joke}
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
